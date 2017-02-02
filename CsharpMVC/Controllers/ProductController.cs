eab1e3f2371267d6d276da6e9bf7ba34955db64a
Saa Maphinda-Lebbie <saamaphinda-lebbie@Saas-Mac-mini.local>
2017-02-01 22:16:07 -0800

    First Commit

---
+++ CsharpMVC/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CsharpMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View ();
+        }
+		public ActionResult ProductDetails()
+		{
+			return View();
+		}
+    }
+}
